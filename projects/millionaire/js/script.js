7363c31809193c256dd59a70d211f65e3797c836
Liliia Novikova <kplily85@gmail.com>
2021-06-17 11:11:48 +0300

    add start modal

--- projects/millionaire/js/script.js
+++ projects/millionaire/js/script.js
@@ -9,12 +9,13 @@ const refs = {
   correctAnswer: document.querySelector('[data-value="correct-answer"]'),
   totalMoney: document.querySelector('[data-value="total-money"]'),
   bank: document.querySelector('[data-value="bank"]'),
-  btnCloseModal: document.querySelector('[data-value="close-modal"]'),
+  // btnCloseModal: document.querySelector('[data-value="close-modal"]'),
   btnTryAgain: document.querySelector('[data-value="btn-try-again"]'),
   btnHelpFriend: document.querySelector('.js-call'),
   btnHelpFifty: document.querySelector('.js-fifty'),
   btnAskAudience: document.querySelector('.js-audience'),
   help: document.querySelector('[data-value="answer"]'),
+  modal: document.querySelector('.modal-footer'),
   gameOverText: document.querySelector('[data-value="js-over-text"]'),
   mins: document.querySelector('[data-value="mins"]'),
   secs: document.querySelector('[data-value="secs"]')
@@ -22,38 +23,56 @@ const refs = {
 
 window.addEventListener('load', renderQuestion);
 refs.optionsList.addEventListener('click', checkAnswer);
-refs.btnCloseModal.addEventListener('click', closeModal);
+// refs.btnCloseModal.addEventListener('click', closeModal);
 refs.btnTryAgain.addEventListener('click', tryAgain);
 refs.btnHelpFriend.addEventListener('click', callToFriend);
 refs.btnHelpFifty.addEventListener('click', helpFiftyFifty);
 refs.btnAskAudience.addEventListener('click', askAudience);
 
+let countdown;
+let timerId;
 let indexId = 0;
 let indexOfQuestion = -1;
 let indexOfPage = 0;
 let score = 0; //result of game
 let arrAfterFifty = [];
-let textWinner = `Вы выиграли 1 000 000! <br/> "Чем больше трудностей в борьбе, тем и победа будет краше". <br/> Лопе де Вега`;
-let textGameOver = `«Никогда не сдавайся, даже когда должен». Келли Криг`;
-let textTimeOver = `Время вышло!`
+let textInModal;
 
 refs.numberOfAllquestion.innerHTML = quizQuestions.length;
 
+const startModal = () => {
+  if (indexOfPage === 0) {
+    const btnStartGame = `<a type="button" class="btn btn-warning" data-value="start-game">Начать игру</a>`
+    refs.help.innerHTML = `Для того, чтобы заработать 1 000 000 $, необходимо правильно ответить на 15 вопросов из различных областей знаний. Каждый вопрос имеет 4 варианта ответа, из которых только один является верным. Каждый вопрос имеет конкретную стоимость.`
+    refs.modal.innerHTML = btnStartGame;
+    const startGame = document.querySelector('[data-value="start-game"]');
+    const modal = document.querySelector('.task-over-modal')
+    modal.classList.add('show');
+    modal.style.backgroundColor = 'rgb(3, 27, 99)'
+    startGame.addEventListener('click', () => {
+      timer()
+      document.querySelector('.task-over-modal').classList.remove('show');
+      startGame.remove();
+      refs.help.innerHTML = '';
+      modal.style.backgroundColor = 'rgba(0, 0, 0, 0.4)'
+    });
+
+    } 
+}
+
 // timer
-let countdown = 15 * 60 * 1000;
-let timerId = setInterval(() => {
+function timer() {
+  countdown = 15 * 60 * 1000;
+  timerId = setInterval(() => {
   countdown -= 1000;
   let mins = String(Math.floor(countdown / (60 * 1000))).padStart(2, '0');
   let secs = String(Math.floor((countdown - (mins * 60 * 1000)) / 1000)).padStart(2, '0');
 
-  if (countdown <= 0) {
-    quizOver(textTimeOver);
-  } else {
     refs.mins.textContent = mins; 
-    refs.secs.textContent = secs; 
-  }
+    refs.secs.textContent = secs;
 
 }, 1000); 
+}
 
 // audio
 function soundClick(src, play) {
@@ -93,13 +112,16 @@ let completedAnswers = [];
 
 function renderQuestion() {
   let index = indexOfQuestion + 1;
+  startModal()
   if (indexOfPage == quizQuestions.length) {
     soundClick('sourse/out-of-commercials-2008.mp3', true)
-    quizOver(textWinner);
   } else if (completedAnswers.length >= 0) {
         indexOfQuestion = index;
         startGame();
     }
+  if(completedAnswers.length >= quizQuestions.length) {
+    quizOver()
+  }
   completedAnswers.push(indexOfQuestion);
   userBank()
 }
@@ -112,12 +134,14 @@ function userBank() {
      refs.bank.innerHTML = moneyInBank
      refs.help.innerHTML = 'Поздравляем! В банке первая несгораемая сумма - 1000$'
      document.querySelector('.task-over-modal').classList.add('show');
+     closeModal()
    }
    if (completedAnswers.length == 11){
     moneyInBank = quizQuestions[9].money
     refs.bank.innerHTML = moneyInBank
     refs.help.innerHTML = 'Поздравляем! Ваш выигрыш увеличился, в банке - 32000$'
     document.querySelector('.task-over-modal').classList.add('show');
+    closeModal()
    } 
 }
 
@@ -148,7 +172,7 @@ if (evt.target.dataset.id == quizQuestions[indexOfQuestion].rightAnswer) {
     soundClick('sourse/khsm_q1-5-wrong.mp3', true);
     setTimeout(() => {
       soundClick('sourse/q6-2000-clock.mp3', true)
-      quizOver(textGameOver);
+      quizOver();
     }, 1000);
     }, 1000);
   }
@@ -192,6 +216,7 @@ function callToFriend() {
     refs.help.innerHTML = `Я думаю ответ <b>${quizQuestions[indexOfQuestion]?.options[randomNum]}</b> &#128515;`;
   }
   document.querySelector('.task-over-modal').classList.add('show');
+  closeModal()
 }
 
 function helpFiftyFifty() {
@@ -278,17 +303,38 @@ function askAudience() {
   
   refs.help.innerHTML = list
   document.querySelector('.task-over-modal').classList.add('show');
+  closeModal()
 }
 
 // game over
-function quizOver(listOver) {
+function quizOver() {
 clearInterval(timerId);
-refs.gameOverText.innerHTML = listOver;
+refs.gameOverText.innerHTML = modalText();
  document.querySelector('.quiz-over-modal').classList.add('show');
 }
 
+function modalText(){
+ if(indexOfPage !== 0 && completedAnswers.length <= 5) {
+    textInModal = `«Никогда не сдавайся, даже когда должен». Келли Криг`;
+  } else if(completedAnswers.length >= 6 && completedAnswers.length < 11){
+    textInModal = 'Поздравляем! Вы выиграли 1000$'
+   } else if (completedAnswers.length >= 11 && completedAnswers.length < 15){
+    textInModal = 'Поздравляем! Вы выиграли 32000$'
+   } else if (completedAnswers.length >= quizQuestions.length){
+    textInModal = `Вы выиграли 1 000 000! <br/> "Чем больше трудностей в борьбе, тем и победа будет краше". <br/> Лопе де Вега`;
+   }
+   if (countdown <= 0) {
+    textInModal = `Время вышло!`
+  }
+   return textInModal;
+}
+
 function closeModal() {
-  document.querySelector('.task-over-modal').classList.remove('show');
+  const btnClose = `<a type="button" class="btn btn-secondary" data-value="close-modal">ok</a>`
+  refs.modal.innerHTML = btnClose;
+  const btnCloseModal = document.querySelector('[data-value="close-modal"]');
+  btnCloseModal.addEventListener('click', () =>  {
+    document.querySelector('.task-over-modal').classList.remove('show')});
 }
 
 function tryAgain() {
